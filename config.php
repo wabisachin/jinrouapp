abc899cc9c82dc477ace4e71ceab2e9975832488
EC2 Default User <ec2-user@ip-172-31-47-28.ap-northeast-1.compute.internal>
2020-04-06 14:16:49 +0000

    クイズアプリ作成

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    ini_set('display_eroors', 1);
+    
+    session_start();
+    require_once(__DIR__ . '/functions.php');
+?>
---
+++ quiz.js
@@ -0,0 +1,37 @@
+$(function(){
+    'use strict';
+    
+    $('.answer').on('click', function(){
+        var $selected = $(this);
+        if ($selected.hasClass('correct') || $selected.hasClass('wrong')){
+            return;
+        }
+        $selected.addClass('selected');
+        var answer = $selected.text();
+        
+        $.post('/_answer.php', {
+            answer: answer, 
+            token: $("#token").val()
+        }).done(function(res){
+            $('.answer').each(function(){
+                if ($(this).text() === res.correct_answer) {
+                    $(this).addClass('correct');
+                } else {
+                    $(this).addClass('wrong');
+                }
+            });
+            if (answer === res.correct_answer){
+                $selected.text(answer + '...correct!')
+            }
+            else {
+                $selected.text(answer + '...wrong!')
+            }
+            $('#btn').removeClass('disabled');
+        });
+        $('#btn').on('click', function(){
+            if (!$(this).hasClass('disabled')){
+                location.reload();
+            }
+        })
+    });
+});
